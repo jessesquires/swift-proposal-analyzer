8147e968c819a85f45ff829a881ffa8ea0a9f1e9
Jesse Squires <jesse.d.squires@gmail.com>
2017-03-12 21:12:55 -0700

    add docs script and build docs

---
+++ build_docs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Docs by jazzy
+# https://github.com/realm/jazzy
+# ------------------------------
+
+#!/bin/bash
+
+# Docs by jazzy
+# https://github.com/realm/jazzy
+# ------------------------------
+
+jazzy \
+    --clean \
+    --author 'Jesse Squires' \
+    --author_url 'https://twitter.com/jesse_squires' \
+    --github_url 'https://github.com/jessesquires/swift-proposal-analyzer' \
+    --module 'ProposalAnalyzer' \
+    --source-directory . \
+    --readme 'README.md' \
+    --output docs/ \
